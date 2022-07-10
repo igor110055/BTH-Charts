ecc6ad7a7418de58848fafb61b005b846ddacfe1
BeyondTheHodl <57193875+BeyondTheHodl@users.noreply.github.com>
2022-07-10 09:23:33 -0400

    Update App.py

--- App.py
+++ App.py
@@ -215,7 +215,7 @@ if chart == 'Volume Profile' and timeframe == 'Daily':
     fig.add_hline(y=poc, line_color="red", annotation_text='POC ' + poc_text, annotation_position="top left")
     fig.add_trace(go.Scatter(x=[today.index[0], today.index[0]],
                              y=[min(current_chart['Low']), max(current_chart['High'])], mode='lines',
-                             line=dict(color='white', width=1, dash='solid'),
+                             line=dict(color='white', width=1, dash='dot'),
                              name='New Day'))
 
     fig.layout.yaxis.showgrid = False

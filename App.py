ea28218988788d59225bc5080b5542beb0190793
BeyondTheHodl <57193875+BeyondTheHodl@users.noreply.github.com>
2022-07-11 21:13:21 -0400

    Update App.py

--- App.py
+++ App.py
@@ -275,10 +275,12 @@ if chart == 'Volume Profile' and timeframe == 'Daily':
                     line_color='yellow',
                     line_dash="dash")
       fig.add_hline(y=poc, line_color="red", annotation_text='POC ' + poc_text, annotation_position="top left")
-      fig.add_trace(go.Scatter(x=[today.index[0], today.index[0]],
-                               y=[min(current_chart['Low']), max(current_chart['High'])], mode='lines',
-                               line=dict(color='white', width=1, dash='dot'),
-                               name='New Day'))
+      
+      if binancetime.hour < 3:
+        fig.add_trace(go.Scatter(x=[today.index[0], today.index[0]],
+                                 y=[min(current_chart['Low']), max(current_chart['High'])], mode='lines',
+                                 line=dict(color='white', width=1, dash='dot'),
+                                 name='New Day'))
 
       fig.update_layout(autosize=False, width=1280, height=720, title_text=str(symbol.upper()) + 'USDT 5min',
                       xaxis_rangeslider_visible=False,
